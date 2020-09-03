1aca6fc10e31ef61f61d6417a070c34c7b501c17
BOBO1997git <yh198595@gmail.com>
2020-09-03 11:09:17 +0900

    added testcase

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,8 +1,34 @@
 package a
 
-func f() {
-	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+import (
+	"fmt"
+)
+
+// S is a fake
+type S struct{}
+
+func (S) Error() error {
+	return fmt.Errorf("this is error")
 }
 
+func fError() error {
+	return fmt.Errorf("this is error")
+}
+
+func main() {
+	if true {
+		fError()
+		return // OK
+	}
+
+	var s S
+	s.Error() // want "NG"
+
+	a := 1
+	if a == 1 {
+		s.Error()
+		return // OK
+	}
+
+	fError() // want "NG"
+}
