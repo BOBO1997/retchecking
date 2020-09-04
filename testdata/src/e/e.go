f4872c350ad5286588e40c5d3d6f9a363af536eb
BOBO1997git <yh198595@gmail.com>
2020-09-04 12:41:40 +0900

    added some edge case in e.go

--- testdata/src/e/e.go
+++ testdata/src/e/e.go
@@ -1,9 +1,32 @@
 package e
 
+import "net/http"
+
+// S is a struct
+type S struct {
+	f func(http.ResponseWriter, string, int)
+}
+
 func main() {
 	err1, err2 := func() {}, func() {}
 	err1() // want "NG"
 	err2() // want "NG"
 
 	func() {}() // want "OK"
+
+	var err3 func()
+	err3 = func() {}
+
+	err3() // want "NG"
+
+	// how about []func() list?
+	errs := make([]func(), 10)
+	errs[5] = func() {}
+	errs[5]() // want "NG"
+
+	var s S
+	var w http.ResponseWriter
+	s.f = http.Error
+
+	s.f(w, "", 0) // want "NG"
 }
