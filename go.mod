50cf9661d3f9fcc094c2dbebd185f56c80e10174
BOBO1997git <yh198595@gmail.com>
2020-09-04 11:38:37 +0900

    fixed the bug on *ast.FuncLit: remaining handler of *ast.AssignStmt

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/BOBO1997/retchecking
 go 1.14
 
 require (
+	github.com/deckarep/golang-set v1.7.1
 	github.com/gostaticanalysis/analysisutil v0.1.0
 	golang.org/x/tools v0.0.0-20200903005429-2364a5e8fdcf
 )
--- retchecking.go
+++ retchecking.go
@@ -6,6 +6,7 @@ import (
 	"go/token"
 	"go/types"
 
+	mapset "github.com/deckarep/golang-set"
 	"github.com/gostaticanalysis/analysisutil"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -29,58 +30,56 @@ func run(pass *analysis.Pass) (interface{}, error) {
 	// list of function to be checked
 
 	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	fileName := pass.Files[0].Name.Name
 	ast.Print(pass.Fset, pass.Files[0].Decls)
 
-	// TODO: use map[types.Object]struct{} instead of []types.Object
-	functions := make([]types.Object, 0)
-	functions = append(functions, analysisutil.LookupFromImports(pass.Pkg.Imports(), "net/http", "Error"))
-	functions = append(functions, analysisutil.ObjectOf(pass, "a", "fError"))
-	//functions = append(functions, pass.Pkg.Scope().Lookup("Error"))
-	functions = append(functions, analysisutil.MethodOf(analysisutil.TypeOf(pass, "a", "S"), "Error"))
-	for _, function := range functions {
-		fmt.Println(function)
+	// DONE: use map[types.Object]struct{} instead of []types.Object
+	funcSet := mapset.NewSet()
+	if obj := analysisutil.LookupFromImports(pass.Pkg.Imports(), "net/http", "Error"); obj != nil {
+		funcSet.Add(obj)
 	}
+	if obj := analysisutil.ObjectOf(pass, fileName, "fError"); obj != nil {
+		funcSet.Add(obj)
+	}
+	if obj := analysisutil.MethodOf(analysisutil.TypeOf(pass, fileName, "S"), "Error"); obj != nil {
+		funcSet.Add(obj)
+	}
+	fmt.Println(funcSet)
+	fmt.Println(funcSet.Cardinality())
+	fmt.Println(funcSet.Contains(analysisutil.LookupFromImports(pass.Pkg.Imports(), "net/http", "Error")))
+	fmt.Println(funcSet.Contains(analysisutil.LookupFromImports(pass.Pkg.Imports(), pass.Files[0].Name.Name, "a")))
 
 	/*
-		fmt.Println("\nDefs")
-		fmt.Println(pass.TypesInfo.Defs)
-		for key, item := range pass.TypesInfo.Defs {
-			fmt.Println("key: ", key, " item: ", item)
-		}
-		fmt.Println("\nScopes")
-		for key, item := range pass.TypesInfo.Scopes {
-			fmt.Println("key: ", key, " item: ", item)
-		}
-		fmt.Println("\nImplicits")
-		for key, item := range pass.TypesInfo.Implicits {
-			fmt.Println("key: ", key, " item: ", item)
-		}
-		fmt.Println("\nSelections")
-		for key, item := range pass.TypesInfo.Selections {
-			fmt.Println("key: ", key, " item: ", item)
-		}
-		fmt.Println("\nInitOrder")
-		for key, item := range pass.TypesInfo.InitOrder {
-			fmt.Println("key: ", key, " item: ", item)
-		}
-		fmt.Println("\nTypes")
-		for key, item := range pass.TypesInfo.Types {
-			fmt.Println("key: ", key, " item: ", item)
-		}
-		fmt.Println("\nUses")
-		for key, item := range pass.TypesInfo.Uses {
-			fmt.Println("key: ", key, " item: ", item)
+		functions := make([]types.Object, 0)
+		functions = append(functions, analysisutil.LookupFromImports(pass.Pkg.Imports(), "net/http", "Error"))
+		functions = append(functions, analysisutil.ObjectOf(pass, "a", "fError"))
+		//functions = append(functions, pass.Pkg.Scope().Lookup("Error"))
+		functions = append(functions, analysisutil.MethodOf(analysisutil.TypeOf(pass, "a", "S"), "Error"))
+		for _, function := range functions {
+			fmt.Println(function)
 		}
 	*/
 
-	// TODO: add a handler for *ast.AssignStmt and closure
+	// TODO: add a handler for *ast.AssignStmt and *ast.FuncLit
+	// TODO: *ast.AssignStmt, add this to funcSet
+	// DONE: *ast.FuncLit, add this to the switch handler
 
+	// depth first search on ast nodes, with referring each element of the list of the node.
+	// time complexity of this part is three times larger than the simple depth first search
 	inspect.Preorder(nil, func(node ast.Node) {
 		switch block := node.(type) {
 		case *ast.BlockStmt:
 			for i, stmt := range block.List {
 				fmt.Println()
 				switch expr := stmt.(type) {
+				case *ast.AssignStmt: // handling with assign statement, := and =
+					for _, v := range expr.Lhs {
+						if obj := pass.TypesInfo.Uses[v.(*ast.Ident)]; obj != nil {
+							fmt.Println(obj)
+							funcSet.Add(obj)
+						}
+						fmt.Println(funcSet)
+					}
 				case *ast.ExprStmt: // if a function is called
 					switch x := expr.X.(type) {
 					case *ast.CallExpr:
@@ -92,6 +91,11 @@ func run(pass *analysis.Pass) (interface{}, error) {
 							fmt.Println("fun.Sel.Obj: ", fun.Sel)
 							xobj = pass.TypesInfo.Uses[fun.Sel]
 							pos = fun.Sel.Pos()
+						case *ast.FuncLit:
+							fmt.Println("fun is an *ast.FuncLit: ", fun)
+							pos = fun.Pos()
+							pass.Reportf(pos, "OK")
+							continue
 						default: // if the function is a normal function
 							//xobj = pass.TypesInfo.Implicits[x]
 							fmt.Println("fun.(*ast.Ident): ", fun.(*ast.Ident))
@@ -102,7 +106,7 @@ func run(pass *analysis.Pass) (interface{}, error) {
 						if i == len(block.List)-1 {
 							fmt.Println("The last function without return", pass.Fset.Position(pos), "\nNG")
 							pass.Reportf(pos, "NG")
-						} else if i < len(block.List)-1 && isObjInList(xobj, functions) {
+						} else if i < len(block.List)-1 && funcSet.Contains(xobj) {
 							switch block.List[i+1].(type) {
 							case *ast.ReturnStmt:
 								fmt.Println(pass.Fset.Position(pos), "\nOK")
@@ -122,9 +126,12 @@ func run(pass *analysis.Pass) (interface{}, error) {
 			}
 		}
 	})
+
+	dumpTypesInfo(pass)
 	return nil, nil
 }
 
+// old lib, unused
 func isObjInList(xobj types.Object, functions []types.Object) bool {
 	for _, obj := range functions {
 		if xobj == obj {
@@ -134,6 +141,7 @@ func isObjInList(xobj types.Object, functions []types.Object) bool {
 	return false
 }
 
+// old lib, unused
 func isStringInList(xobj string, functions []string) bool {
 	for _, obj := range functions {
 		if xobj == obj {
@@ -141,4 +149,37 @@ func isStringInList(xobj string, functions []string) bool {
 		}
 	}
 	return false
+}
+
+// for debugging, unused
+func dumpTypesInfo(pass *analysis.Pass) {
+	fmt.Println("\nDefs")
+	fmt.Println(pass.TypesInfo.Defs)
+	for key, item := range pass.TypesInfo.Defs {
+		fmt.Println("key: ", key, " item: ", item)
+	}
+	fmt.Println("\nScopes")
+	for key, item := range pass.TypesInfo.Scopes {
+		fmt.Println("key: ", key, " item: ", item)
+	}
+	fmt.Println("\nImplicits")
+	for key, item := range pass.TypesInfo.Implicits {
+		fmt.Println("key: ", key, " item: ", item)
+	}
+	fmt.Println("\nSelections")
+	for key, item := range pass.TypesInfo.Selections {
+		fmt.Println("key: ", key, " item: ", item)
+	}
+	fmt.Println("\nInitOrder")
+	for key, item := range pass.TypesInfo.InitOrder {
+		fmt.Println("key: ", key, " item: ", item)
+	}
+	fmt.Println("\nTypes")
+	for key, item := range pass.TypesInfo.Types {
+		fmt.Println("key: ", key, " item: ", item)
+	}
+	fmt.Println("\nUses")
+	for key, item := range pass.TypesInfo.Uses {
+		fmt.Println("key: ", key, " item: ", item)
+	}
 }
